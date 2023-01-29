320695f39df752416dcde35005644153f396b98b
ryhhill1998 <ryhhill1998@outlook.com>
2023-01-29 18:09:18 +0000

    Gave pieces functionality to allow them to move by clicking

--- game.js
+++ game.js
@@ -1,20 +1,16 @@
+// DOM elements
 const board = document.getElementById("board");
-let selectedSpace = null;
 
-const highlightSpace = ({target}) => {
-    const space = target.closest(".space");
-    removeSpaceHighlight(selectedSpace);
-    if (selectedSpace === space) {
-        selectedSpace = null;
-    } else {
-        space.style.borderColor = "red";
-        selectedSpace = space;
-    }
+// game variables
+let pieceSelected, spaceSelected = null;
+let turn = "white";
+
+const highlightSpace = (space) => {
+    space.style.borderColor = "red";
+    spaceSelected = space;
 }
 
 const removeSpaceHighlight = (space) => {
-    if (!space) return;
-
     if (space.classList.contains("space-blue")) {
         space.style.borderColor = "#3A8891";
     } else {
@@ -22,4 +18,28 @@ const removeSpaceHighlight = (space) => {
     }
 }
 
-board.addEventListener("click", highlightSpace);
+const spaceClicked = ({target}) => {
+    console.log(turn);
+    const space = target.closest(".space");
+    const image = getImage(space);
+
+    if (!image && pieceSelected) {
+        spaceSelected.removeChild(pieceSelected);
+        const newImage = document.createElement("img");
+        space.appendChild(newImage);
+        newImage.src = pieceSelected.src;
+        removeSpaceHighlight(spaceSelected);
+        spaceSelected = pieceSelected = null;
+
+        turn = turn === "white" ? "black" : "white";
+
+    } else if (correctColourClicked(image?.src)) {
+        pieceSelected = image;
+        highlightSpace(space);
+    }
+}
+
+const getImage = (space) => space.querySelector("img")
+const correctColourClicked = (imageSrc) => imageSrc?.includes(turn);
+
+board.addEventListener("click", spaceClicked);
